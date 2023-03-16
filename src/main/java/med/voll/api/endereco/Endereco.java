3cb1e0b2809adfc2f587796b9b24187655374e68
Alexandre Toshiro <toshiro.lyn@gmail.com>
2023-03-15 21:29:25 -0300

    Criando a interface de repositorio e mapeando os DTOs para as entidades

--- src/main/java/med/voll/api/endereco/Endereco.java
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Endereco {
 
-    private String Logradouro;
+    private String logradouro;
     private String bairro;
     private String cep;
     private String numero;
@@ -19,4 +19,13 @@ public class Endereco {
     private String cidade;
     private String uf;
 
+    public Endereco(DadosEndereco dados) {
+        this.logradouro = dados.logradouro();
+        this.bairro = dados.bairro();
+        this.cep = dados.cep();
+        this.numero = dados.numero();
+        this.complemento = dados.complemento();
+        this.cidade = dados.cidade();
+        this.uf = dados.uf();
+    }
 }
